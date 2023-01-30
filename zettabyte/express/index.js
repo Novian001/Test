a833e2a3a04e931fbe29e75ce6cb571c20a785cb
Muhammad Alif Rahmat Novian Arsianto <kingboard154@gmail.com>
2023-01-30 15:00:46 +0700

    update

--- zettabyte/express/index.js
+++ zettabyte/express/index.js
@@ -27,32 +27,44 @@ const commentSchema = new mongoose.Schema({
 
 const Comment = mongoose.model("Comment", commentSchema);
 
+function sendResponse(res, statusCode, data, message) {
+  return res.status(statusCode).json({
+    meta: {
+      code: statusCode,
+      status: statusCode >= 200 && statusCode < 300 ? "success" : "error",
+      message: message || "",
+    },
+    data,
+  });
+}
+
 app.get("/articles", async (req, res) => {
   const articles = await Article.find();
-  res.json(articles);
+  sendResponse(res, 200, articles);
 });
 
 app.get("/articles/:id", async (req, res) => {
   const article = await Article.findById(req.params.id);
-  res.json(article);
+  sendResponse(res, 200, article);
 });
 
 app.post("/articles", async (req, res) => {
   const article = new Article(req.body);
   await article.save();
-  res.json(article);
+  sendResponse(res, 200, article, "Article created");
 });
 
 app.put("/articles/:id", async (req, res) => {
   const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
-  res.json(article);
+  sendResponse(res, 200, article, "Article updated");
 });
 
 app.delete("/articles/:id", async (req, res) => {
-  await Article.findByIdAndDelete(req.params.id);
-  res.json({ message: "Deleted" });
+  const id = req.params.id;
+  const deletedArticle = await Article.findByIdAndDelete(id);
+  sendResponse(res, 200, deletedArticle, "Article deleted");
 });
 
 app.get("/comments", async (req, res) => {
