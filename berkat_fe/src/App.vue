59d06d86bff7d94be5aed6ce98b64f4604e4c081
Muhammad Alif Rahmat Novian Arsianto <kingboard154@gmail.com>
2023-02-15 12:44:39 +0700

    new

---
+++ berkat_fe/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="movie in movies" :key="movie.title">
+        title = {{ movie.title }}
+      </li>
+      <li v-for="movie in movies" :key="movie.release_date">
+        release =
+        {{ movie.release_date }}
+      </li>
+      <li v-for="movie in movies" :key="movie.popularity">
+        popularity =
+        {{ movie.popularity }}
+      </li>
+      <li v-for="movie in movies" :key="movie.vote_average">
+        vote =
+        {{ movie.vote_average }}
+      </li>
+      <li v-for="movie in movies" :key="movie.category_id">
+        category =
+        {{ movie.category_id }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://127.0.0.1:8000/api/now-playing")
+      .then((response) => {
+        this.movies = response.data.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
---
+++ berkat_fe/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
